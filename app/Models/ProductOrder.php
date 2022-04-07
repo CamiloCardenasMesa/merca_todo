594bdc105f8b4bb47cfe9d6f786ab445432a7746
Camilo <arqcamilocardenasmesa@gmail.com>
2022-04-07 00:44:11 -0500

    fix(model): change relationships and name

---
+++ app/Models/ProductOrder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class ProductOrder extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'quantity',
+    ];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product(): HasOne
+    {
+        return $this->hasOne(Product::class);
+    }
+}
