5d4a4a745097252175c17f18eea9ac95779b145e
Camilo <arqcamilocardenasmesa@gmail.com>
2022-04-07 00:49:44 -0500

    feat: retry payment and show

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
-use App\Models\OrderProduct;
+use App\Models\ProductOrder;
 use App\Services\WebcheckoutService;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Str;
+use Illuminate\View\View;
 
 class OrderController extends Controller
 {
@@ -27,11 +28,11 @@ class OrderController extends Controller
         $order->save();
 
         foreach ($cart as $cartItem) {
-            $orderProduct = new OrderProduct();
+            $productOrder = new ProductOrder();
 
-            $orderProduct->quantity = $cartItem->qty;
-            $orderProduct->order_id = $order->id;
-            $orderProduct->product_id = $cartItem->id;
+            $productOrder->quantity = $cartItem->qty;
+            $productOrder->order_id = $order->id;
+            $productOrder->product_id = $cartItem->id;
         }
 
         $webcheckoutService = new WebcheckoutService();
@@ -45,14 +46,82 @@ class OrderController extends Controller
                 'reference' => $order->id,
             ],
             'expiration' => date('c', strtotime('+2 days')),
-            'returnUrl' => route('dashboard'), //ruta api para actualizar orden + referencia
+            'returnUrl' => route('buyer.orders.show', $order->id),
         ]);
-        /* dd($response); */
+
         $order->session_id = $response['requestId'];
         $order->process_url = $response['processUrl'];
 
         $order->save();
 
+        Cart::destroy();
+
         return Redirect::to($order->process_url);
+    }
+
+    public function show(Order $order): View
+    {
+        if ($order->state === 'PENDING') {
+            $webcheckoutService = new WebcheckoutService();
+
+            $response = $webcheckoutService->getInformation($order->session_id);
+            if ($response['status']['status'] !== 'PENDING') {
+                $order->state = $response['status']['status'];
+
+                $order->save();
+            }
+        }
+
+        return view('buyer.orders.show', compact('order'));
+    }
+
+    public function retry(Order $order): RedirectResponse
+    {
+        /*  dd($order->productsOrder()); */
+        $newOrder = new Order();
+
+        $newOrder->reference = Str::uuid();
+        $newOrder->currency = 'COP';
+        $newOrder->state = 'PENDING';
+        $newOrder->total = $order->total;
+        $newOrder->user_id = Auth::user()->id;
+
+        $newOrder->save();
+
+        foreach ($order->productsOrder() as $productOrder) {
+            $productNewOrder = new ProductOrder();
+
+            $productNewOrder->quantity = $productOrder->qty;
+            $productNewOrder->newOrder_id = $newOrder->id;
+            $productNewOrder->product_id = $productOrder->product_id;
+        }
+
+        $webcheckoutService = new WebcheckoutService();
+
+        $response = $webcheckoutService->createSession([
+            'payment' => [
+                'amount' => [
+                    'currency' => $newOrder->currency,
+                    'total' => $newOrder->total,
+                ],
+                'reference' => $newOrder->id,
+            ],
+            'expiration' => date('c', strtotime('+2 days')),
+            'returnUrl' => route('buyer.orders.show', $newOrder->id),
+        ]);
+
+        $newOrder->session_id = $response['requestId'];
+        $newOrder->process_url = $response['processUrl'];
+
+        $newOrder->save();
+
+        return Redirect::to($newOrder->process_url);
+    }
+
+    public function index(): View
+    {
+        $orders = Order::where('user_id', auth()->user()->id)->get();
+
+        return view('buyer.orders.index', compact('orders'));
     }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
@@ -22,5 +23,10 @@ class Order extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function productsOrder(): HasMany
+    {
+        return $this->hasMany(ProductOrder::class);
     }
 }
