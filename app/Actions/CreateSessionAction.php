766fb94d483387b7b69ec1d039d5278f15e496d6
Camilo <arqcamilocardenasmesa@gmail.com>
2022-04-18 15:26:14 -0500

    feat: create session action

---
+++ app/Actions/CreateSessionAction.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\Order;
+use App\Services\WebcheckoutService;
+
+class CreateSessionAction
+{
+    public static function execute(WebcheckoutService $webcheckoutService, Order $order): array
+    {
+        $response = $webcheckoutService->createSession([
+            'payment' => [
+                'amount' => [
+                    'currency' => $order->currency,
+                    'total' => $order->total,
+                ],
+                'reference' => str_pad($order->id, 6, '0', STR_PAD_LEFT),
+            ],
+            'expiration' => date('c', strtotime('+2 days')),
+            'returnUrl' => route('buyer.orders.show', $order->id),
+        ]);
+
+        return $response;
+    }
+}
