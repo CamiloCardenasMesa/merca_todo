8d8bd6cfbc0a300b02702ee9edbbbc5726f68874
Camilo <arqcamilocardenasmesa@gmail.com>
2022-04-18 16:11:08 -0500

    feat: justify text

--- resources/views/buyer/orders/index.blade.php
+++ resources/views/buyer/orders/index.blade.php
@@ -11,9 +11,9 @@
                 <div class="p-8 bg-white border-b border-gray-200">
                     <table class="container">
                         <thead>
-                            <tr class="bg-gray-100">
+                            <tr class="bg-gray-100 text-center">
                                 <th class="border border-gray-400 px-4 py-2">Referencia</th>
-                                <th class="border border-gray-400 px-4 py-2">Estado</th>
+                                <th class="border border-gray-400 px-4 py-2">Estado de la transacción</th>
                                 <th class="border border-gray-400 px-4 py-2">Total</th>
                                 <th class="border border-gray-400 px-4 py-2">Fecha de Creación</th>
                                 <th class="border border-gray-400 px-4 py-2">Fecha de actualización</th>
@@ -22,12 +22,12 @@
                         </thead>  
                         @foreach ($orders as $order)
                             <tbody>
-                                <tr>
-                                    <td class="border border-gray-400 px-4 py-2 text-left">{{ $order->reference = str_pad($order->id, 6, '0', STR_PAD_LEFT) }}</td>
-                                    <td class="border border-gray-400 px-4 py-2 text-left">{{ $order->state }}</td>
-                                    <td class="border border-gray-400 px-4 py-2 text-left">{{ $order->total }}</td>
-                                    <td class="border border-gray-400 px-4 py-2 text-left">{{ $order->created_at }}</td>
-                                    <td class="border border-gray-400 px-4 py-2 text-left">{{ $order->updated_at }}</td>
+                                <tr class="text-center">
+                                    <td class="border border-gray-400 px-4 py-2">{{ $order->reference = str_pad($order->id, 6, '0', STR_PAD_LEFT) }}</td>
+                                    <td class="border border-gray-400 px-4 py-2">{{ $order->state }}</td>
+                                    <td class="border border-gray-400 px-4 py-2">{{ $order->total }}</td>
+                                    <td class="border border-gray-400 px-4 py-2">{{ $order->created_at }}</td>
+                                    <td class="border border-gray-400 px-4 py-2">{{ $order->updated_at }}</td>
 
                                     <td class="border border-gray-400 px-4 py-2 text-center">
                                         <x-button-link href="{{ route('buyer.orders.show', $order) }}">Ver</x-button-link>
