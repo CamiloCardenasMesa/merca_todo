e792422c367c3310a62bead951173ec0d99f6a6c
Camilo <arqcamilocardenasmesa@gmail.com>
2022-05-03 00:40:50 -0500

    feat: add methods and request for users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,18 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Admin\Users\UserRequest;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
     public function index(Request $request): View
     {
         if ($request->query('query')) {
-            $users = User::where('name', 'like', '%' . $request->query('query') . '%')
-            ->orwhere('email', 'like', '%' . $request->query('query') . '%')
+            $users = User::where('name', 'like', '%'.$request->query('query').'%')
+            ->orwhere('email', 'like', '%'.$request->query('query').'%')
             ->paginate(10);
         } else {
             $users = User::paginate(10);
@@ -25,6 +27,28 @@ class UserController extends Controller
     public function show(User $user): View
     {
         return view('admin.users.show', compact('user'));
+    }
+
+    public function create()
+    {
+        $roles = Role::pluck('name', 'name')->all();
+
+        return view('admin.users.create', compact('roles'));
+    }
+
+    public function store(UserRequest $request): RedirectResponse
+    {
+        $user = new User();
+
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = $request->input('password');
+        $user->assignRole($request->input('roles'));
+
+        $user->save();
+
+        return redirect()->route('admin.users.index')
+            ->with('success', 'User created successfully');
     }
 
     public function edit(User $user): View
