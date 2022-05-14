9b37f2eb3e43fcf1cff4259ef33254d800c3287b
Camilo <arqcamilocardenasmesa@gmail.com>
2022-05-14 00:51:05 -0500

    test: test added

---
+++ tests/Feature/admin/Roles/CreateRolesTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature\admin\Roles;
+
+use App\Constants\Permissions;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Spatie\Permission\Models\Permission;
+use Tests\TestCase;
+
+class CreateRolesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testAdminUserCanShowCreateRoleView(): void
+    {
+        //Arrange
+        $UserPermission = Permission::create([
+            'name' => Permissions::ROLE_CREATE,
+        ]);
+
+        $adminRole = Role::create(['name' => 'admin'])
+        ->givePermissionTo($UserPermission);
+
+        $admin = User::factory()
+            ->create()
+            ->assignRole($adminRole);
+
+        //Act or Request
+        $response = $this->actingAs($admin)
+            ->get(route('roles.create'));
+
+        //Assert
+        $response->assertOk();
+        $response->assertViewIs('roles.create');
+        $this->assertAuthenticated();
+    }
+
+    public function testAdminUserCanCreateRole(): void
+    {
+        //Arrange
+        $updatePermission = Permission::create([
+            'name' => Permissions::ROLE_CREATE, ]);
+
+        $adminRole = Role::create(['name' => 'admin'])
+            ->givePermissionTo($updatePermission);
+
+        $admin = User::factory()->create()->assignRole($adminRole);
+
+        $userCreated = User::factory()->create();
+
+        //Act or Request
+        $response = $this->actingAs($admin)->post(route('roles.store', $userCreated), [
+            'name' => 'guest',
+            'permission' => Permissions::ROLE_CREATE,
+        ]);
+
+        $userCreated->fresh();
+
+        //Assert
+        $response->assertRedirect('roles');
+        $this->assertNotEquals($userCreated, $admin);
+        $this->assertAuthenticated();
+    }
+}
---
+++ tests/Feature/admin/Roles/UpdateRolesTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature\admin\Roles;
+
+use App\Constants\Permissions;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Spatie\Permission\Models\Permission;
+use Tests\TestCase;
+
+class UpdateRolesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testAdminUserEditRoles(): void
+    {
+        //Arrange
+        $updatePermission = Permission::create([
+            'name' => Permissions::ROLE_EDIT, ]);
+
+        $adminRole = Role::create(['name' => 'admin'])
+            ->givePermissionTo($updatePermission);
+
+        $admin = User::factory()->create()->assignRole($adminRole);
+
+        $userUpdated = Role::create(['name' => 'guest']);
+
+        //Act or Request
+        $response = $this->actingAs($admin)->put(route('roles.update', $userUpdated), [
+            'name' => 'guest',
+            'permission' => Permissions::ROLE_EDIT,
+        ]);
+
+        $userUpdated = $userUpdated->fresh();
+
+        //Assert
+        $response->assertRedirect('/roles');
+        $this->assertEquals($userUpdated->name, 'guest');
+        $this->assertAuthenticated();
+    }
+}
