86f0432c7c5449d10cca67b121ad6ca24d46860b
Camilo <arqcamilocardenasmesa@gmail.com>
2023-09-08 16:48:00 -0500

    style: change name section

--- resources/lang/en/navigation.php
+++ resources/lang/en/navigation.php
@@ -3,7 +3,7 @@
 return [
     'welcome' => 'HOME',
     'users' => 'USERS',
-    'product_list' => 'PRODUCTS LIST',
+    'product_list' => 'PRODUCTS ',
     'orders' => 'ORDERS',
     'roles' => 'ROLES',
 ];
