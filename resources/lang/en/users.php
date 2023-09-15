e74b1daf24ecc4481853698d3c0b470dff9849d7
Camilo <arqcamilocardenasmesa@gmail.com>
2023-09-15 17:52:13 -0500

    style: php cs fixer

--- resources/lang/en/users.php
+++ resources/lang/en/users.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'users_list' => 'Users list', 
+    'users_list' => 'Users list',
     'permissions_of' => 'Permissions of: ',
     'name' => 'Name',
     'user' => 'User: ',
--- resources/lang/es/users.php
+++ resources/lang/es/users.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'users_list' => 'Lista de usuarios', 
+    'users_list' => 'Lista de usuarios',
     'permissions_of' => 'Permisos de: ',
     'name' => 'Nombre',
     'user' => 'Usuario: ',
