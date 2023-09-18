7aa0dfede93bf61ccf2a4894c2c1125af5a2286a
Camilo <arqcamilocardenasmesa@gmail.com>
2023-09-17 21:12:05 -0500

    refactor: change factory password

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,7 +14,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'enable' => true,
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => 'password123', // password
             'remember_token' => Str::random(10),
         ];
     }
