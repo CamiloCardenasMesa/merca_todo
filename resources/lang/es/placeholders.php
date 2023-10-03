0cc0e11d8206a2331016a77841141e4037ef9504
Camilo <arqcamilocardenasmesa@gmail.com>
2023-10-03 16:57:57 -0500

    style: rename value translations

--- resources/lang/es/placeholders.php
+++ resources/lang/es/placeholders.php
@@ -4,5 +4,5 @@ return [
     'welcome_search' => 'Busca tu instrumento',
     'user_search' => 'Busca un usuario',
     'role_search' => 'Busca un Rol',
-    'create_role' => 'Dale un nombre a tu nuevo Rol',
+    'create_role' => 'Nombre del nuevo Rol',
 ];
