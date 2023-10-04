f3fe31aeecbd1db45aabf62223838d87ba4bcb84
Camilo <arqcamilocardenasmesa@gmail.com>
2023-10-04 18:38:49 -0500

    feat: footer added

--- resources/lang/en/navigation.php
+++ resources/lang/en/navigation.php
@@ -7,4 +7,6 @@ return [
     'product_list' => 'Products ',
     'orders' => 'Orders',
     'roles' => 'Roles',
+    'footer_messagge' => 'This project has been built in PHP and Laravel in order to strengthen my web development skills.',
+    'footer_rights' => '. All Rights Reserved.',
 ];
--- resources/lang/es/navigation.php
+++ resources/lang/es/navigation.php
@@ -7,4 +7,6 @@ return [
     'product_list' => 'Productos',
     'orders' => 'Órdenes de compra',
     'roles' => 'Roles',
+    'footer_messagge' => 'Este proyecto ha sido construido en PHP y Laravel con el objetivo de fortalecer mis habilidades en desarrollo web.',
+    'footer_rights' => '. Todos los derechos reservados.',
 ];
