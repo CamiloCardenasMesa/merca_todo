093ae9ef736491489705e6e083c14d2b5361ba21
Camilo <arqcamilocardenasmesa@gmail.com>
2023-10-10 19:28:36 -0500

    test: delete images from storage

---
+++ tests/Feature/DeleteImageTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Storage;
+
+class DeleteImageTest extends TestCase
+{
+    public function testDeleteAllImages()
+    {
+        Artisan::call('images:delete-all');
+
+        $this->assertSame(0, Artisan::call('images:delete-all'));
+
+        $directory = storage_path('app/public/product_images');
+        $files = scandir($directory);
+
+        $this->assertCount(2, $files);
+    }
+
+    public function testDeleteSingleImage()
+    {
+        $disk = Storage::disk('images');
+        $imagePath = 'product_images/test_image.jpg';
+        $disk->put($imagePath, 'contents');
+
+        Artisan::call('images:delete-all');
+
+        $this->assertFalse($disk->exists($imagePath));
+    }
+}
+
+
+
+
+
