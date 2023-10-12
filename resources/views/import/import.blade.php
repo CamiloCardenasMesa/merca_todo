74d90dff4fcfd154bf481299d22b30685f26064c
Camilo <arqcamilocardenasmesa@gmail.com>
2023-10-11 21:21:15 -0500

    style: use existing components on view

--- resources/views/import/import.blade.php
+++ resources/views/import/import.blade.php
@@ -6,29 +6,23 @@
         </div>
     </x-slot>
 
-    <div class="bg-gray-100 min-h-screen pb-9">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex justify-between items-center pb-4">
-                <div class="p-8 bg-white border-b border-gray-200">
-                    @if (isset($errors) && $errors->any())
-                        <div class="alert alert-danger mb-10 " role="alert">
-                            @foreach ($errors->all() as $error)
-                                {{$error}}
-                            @endforeach
-                        </div>
-                    @endif
-                    <div>
-                        <x-auth-session-status class="mb-4" :status="session('status')" />
-                    </div>
-                    <form action="{{ route('admin.products.import.store') }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        <div class="form-group">
-                            <input type="file" name="import_file">
-                            <x-button class="ml-2">IMPORT</x-button>
-                        </div>
-                    </form>
-                </div>
+    <x-article-layout>
+        @if (isset($errors) && $errors->any())
+            <div class="alert alert-danger mb-10 " role="alert">
+                @foreach ($errors->all() as $error)
+                    {{$error}}
+                @endforeach
             </div>
+        @endif
+        <div>
+            <x-auth-session-status class="mb-4" :status="session('status')" />
         </div>
-    </div>
+        <form action="{{ route('admin.products.import.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <input type="file" name="import_file">
+                <x-button class="ml-2">IMPORT</x-button>
+            </div>
+        </form>
+    </x-article-layout>
 </x-app-layout>
