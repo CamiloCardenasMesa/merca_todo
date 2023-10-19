a3439b2dcd5e1b2e5a1d777245958b0184087bfb
Camilo <arqcamilocardenasmesa@gmail.com>
2023-10-19 12:18:08 -0500

    style: buttons colors

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -1,4 +1,4 @@
-<a {{ $attributes->merge(['class' => 'inline-flex items-center pl-3 pr-4 py-2 bg-[#212130] border border-transparent rounded-xl font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-green-400 focus:ring ring-green-400 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+<a {{ $attributes->merge(['class' => 'inline-flex items-center pl-3 pr-4 py-2 bg-[#00B4CC] border-2 border-[#4b4b6e] rounded-xl font-semibold text-xs text-white uppercase tracking-widest hover:bg-[#F5E37F] hover:text-[#4b4b6e] hover:border-[#4b4b6e] transition ease-in-out duration-300']) }}>
     <div class="mr-1">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,4 +1,4 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center pr-3 pl-4 py-2 bg-[#212130] border border-transparent rounded-xl font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-green-400 focus:ring ring-green-400 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center pr-3 pl-4 py-2 bg-[#212130] border-2 border-gray-100 rounded-xl font-semibold text-xs text-white uppercase tracking-widest hover:bg-[#F5E37F] hover:text-[#4b4b6e] hover:border-[#4b4b6e] hover:border-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
     <div class="ml-1">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
