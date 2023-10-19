2f5a5ab4cd3bb6e0453cda2c1efe99d6ab134ed7
Camilo <arqcamilocardenasmesa@gmail.com>
2023-10-19 12:15:56 -0500

    style: table buttons colors changed

--- resources/views/components/delete-button.blade.php
+++ resources/views/components/delete-button.blade.php
@@ -3,7 +3,7 @@
 <form action="{{ $route }}" method="POST">
     @csrf
     @method('DELETE')
-    <button onclick="{{ $textConfirm }}"  class="flex p-2 border shadow border-gray-500 rounded-xl hover:rounded-3xl hover:bg-red-700 hover:text-white hover:border-white transition-all duration-300 text-gray-500">
+    <button onclick="{{ $textConfirm }}" class="inline-flex p-2 my-2 border-2 border-[#00B4CC] text-[#00B4CC] hover:bg-[#F5E37F] hover:text-[#4b4b6e] hover:border-[#4b4b6e] hover:rounded-3xl rounded-xl transition-all duration-300">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
         </svg>
