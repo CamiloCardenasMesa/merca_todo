3ff5bc9b7f0e28b91e668dd5d5d0712eb3b532a5
Camilo <arqcamilocardenasmesa@gmail.com>
2023-11-03 18:15:54 -0500

    style: margin top of login layout

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -1,7 +1,7 @@
 @auth
     @if ($userHasPermissions)
         <aside id="nav-menu" class="text-white md:block hidden bg-aside-gray px-3 lg:px-6 w-80"> 
-            <div class="flex items-center justify-center my-3 lg:my-0">
+            <div class="flex items-center justify-center my-4 lg:my-0">
                 @include('layouts.login')
             </div>
             <div>
