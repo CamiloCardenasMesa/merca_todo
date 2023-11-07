f058456013b4c56864fb2b484a19de2308d97e7f
Camilo <arqcamilocardenasmesa@gmail.com>
2023-11-07 16:50:48 -0500

    WIP: import function

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -19,10 +19,10 @@
                             /> 
                         @endcan
                         @can(App\Constants\Permissions::PRODUCT_EDIT)
-                            <x-button-actions
+                            {{-- <x-button-actions
                                 route="{{ route('admin.products.import') }}" 
                                 svgPath='<path stroke-linecap="round" stroke-linejoin="round" d="M7.5 7.5h-.75A2.25 2.25 0 004.5 9.75v7.5a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25v-7.5a2.25 2.25 0 00-2.25-2.25h-.75m-6 3.75l3 3m0 0l3-3m-3 3V1.5m6 9h.75a2.25 2.25 0 012.25 2.25v7.5a2.25 2.25 0 01-2.25 2.25h-7.5a2.25 2.25 0 01-2.25-2.25v-.75" />' 
-                            />
+                            /> --}}
                             <x-button-actions
                                 route="{{ route('admin.products.export') }}" 
                                 svgPath='<path stroke-linecap="round" stroke-linejoin="round" d="M7.5 7.5h-.75A2.25 2.25 0 004.5 9.75v7.5a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25v-7.5a2.25 2.25 0 00-2.25-2.25h-.75m0-3l-3-3m0 0l-3 3m3-3v11.25m6-2.25h.75a2.25 2.25 0 012.25 2.25v7.5a2.25 2.25 0 01-2.25 2.25h-7.5a2.25 2.25 0 01-2.25-2.25v-.75" />' 
