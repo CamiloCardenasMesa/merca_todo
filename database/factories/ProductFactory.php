4970f24d884f6aa9426959a384f931d2b75875f0
Camilo <95107808+CamiloCardenasMesa@users.noreply.github.com>
2023-11-08 21:45:37 -0500

    fix: pipeline error on ProductFactory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -13,19 +13,20 @@ class ProductFactory extends Factory
 
     public function definition(): array
     {
-        $instrumentsPath = public_path('images\instruments');
+        $instrumentsPath = public_path('images/instruments');
 
-        $imageFiles = glob($instrumentsPath . '\*.{jpg,png,gif}', GLOB_BRACE);
-
-        $randomImage = $imageFiles[array_rand($imageFiles)];
-
+        $imageFiles = glob($instrumentsPath . '/*.{jpg,png,gif}', GLOB_BRACE);
+        
+        $randomImage = !empty($imageFiles) ? $imageFiles[array_rand($imageFiles)] : null;
+        
         return [
             'name' => $this->faker->sentence(3, true),
             'description' => $this->faker->paragraph(10, 10),
             'price' => $this->faker->randomNumber(6),
             'stock' => $this->faker->numberBetween(2, 10),
             'enable' => true,
-            'image' => Storage::disk('images')->putFile('product_images', new File($randomImage)),
+            'image' => $randomImage ? Storage::disk('images')->putFile('product_images', new File($randomImage)) : null,
         ];
+        
     }
 }
